e0bc76807c6439eba05588ae9f2df51feaa51b8b
Adam Touil <162053830+ghazelian@users.noreply.github.com>
2026-03-26 20:44:04 +0100

    MUI & Bootstrap implementations for TP4 branch

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./features/auth/AuthContext";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
